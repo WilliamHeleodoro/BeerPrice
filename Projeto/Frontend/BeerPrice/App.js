11af22f540b66e15e9391fd9b416f95555a396e3
WilliamHeleodoro <williamheleodoro@gmail.com>
2023-08-04 15:33:27 -0300

    Criado projeto frontend

---
+++ Projeto/Frontend/BeerPrice/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {View, Text} from 'react-native';
+
+function App(){
+  return(
+    <View style={{ marginTop: 100 }}>
+      <Text style={{color: '#FF0000'}}>Programador</Text>
+    </View>
+  )
+}
+
+export default App;
